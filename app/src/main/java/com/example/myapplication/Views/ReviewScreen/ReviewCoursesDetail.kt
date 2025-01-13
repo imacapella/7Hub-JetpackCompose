55030cbd75af3d3f4d5794a83ec87a53e357bfef
Zehra Ergöktaş <zehranurergoktas23@gmail.com>
2025-01-13 19:45:25 +0300

    ReviewCoursesDetailScreen finished

--- app/src/main/java/com/example/myapplication/Views/ReviewScreen/ReviewCoursesDetail.kt
+++ app/src/main/java/com/example/myapplication/Views/ReviewScreen/ReviewCoursesDetail.kt
@@ -1,4 +1,5 @@
 package com.example.myapplication.Views.ReviewScreen
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -25,6 +26,12 @@ import androidx.compose.runtime.*
 import com.example.myapplication.ui.theme.CustomShadowButton
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.foundation.background
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.border
+import androidx.compose.ui.text.style.TextOverflow
+
+
+
 
 @Composable
 fun CourseReviewItem(review: Review) {
@@ -39,25 +46,34 @@ fun CourseReviewItem(review: Review) {
                 .padding(8.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.profilepictureanonim),
+                contentDescription = "Profile Picture",
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+            )
             Spacer(modifier = Modifier.width(16.dp))
             Column(modifier = Modifier.weight(1f)) {
+
                 Text(
-                    text = "Anonim", // Yorumlar anonim olarak görünüyor
-                    style = TextStyle(
+                    text =  "Anonim", // Her zaman "Anonim"
+                    style = TextStyle(fontFamily = OpenSansSemiBold,
                         fontWeight = FontWeight.SemiBold,
                         fontSize = 15.sp,
-                        color = Color(0xFF1A181C)
-                    ),
-                )
+                        color = Color(0xFF1A181C) ),
+
+
+                    )
 
                 Spacer(modifier = Modifier.height(4.dp))
 
                 Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
                     repeat(5) { index ->
-                        val starIcon = if (index < review.rating) {
-                            R.drawable.ic_star_filled
-                        } else {
-                            R.drawable.ic_star_outline
+                        val starIcon = when {
+                            index < review.rating -> R.drawable.ic_star_filled
+                            index == review.rating.toInt() && review.rating % 1f != 0f -> R.drawable.ic_star_half
+                            else -> R.drawable.ic_star_outline
                         }
                         Icon(
                             painter = painterResource(id = starIcon),
@@ -67,33 +83,29 @@ fun CourseReviewItem(review: Review) {
                         )
                     }
                 }
-
                 Spacer(modifier = Modifier.height(5.dp))
-
                 Text(
                     text = review.content,
                     style = TextStyle(
-                        fontWeight = FontWeight.SemiBold,
-                        fontSize = 15.sp,
-                        color = Color(0xFF707070)
+                        fontFamily = OpenSansSemiBold,  // Yorumlar için OpenSansSemiBold fontu
+                        fontWeight = FontWeight.SemiBold,  // Yorumlar için yarı kalın yazı
+                        fontSize = 15.sp,  // Yorum font boyutunu belirliyoruz
+                        color = Color(0xFF707070)  // Yorumların rengini değiştirebiliriz
                     )
                 )
-
                 Spacer(modifier = Modifier.height(4.dp))
             }
         }
         Divider(color = Color(0xFFBFBFBF), thickness = 1.dp, modifier = Modifier.padding(horizontal = 16.dp))
     }
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CourseDetailsScreen(
     course: Course,
     onNavigateBack: () -> Unit,
     onRateCourseClick: () -> Unit
-
-
-
 ) {
     var selectedTab by remember { mutableStateOf("Courses") }
     Scaffold(
@@ -101,16 +113,21 @@ fun CourseDetailsScreen(
         topBar = {
             TopAppBar(
                 title = {
-                    Text(
-                        text = "Reviews",
-                        style = TextStyle(
-                            fontWeight = FontWeight.SemiBold,
-                            fontSize = 25.sp,
-                            color = Color(0xFF5F5464)
-                        ),
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .offset(x = 100.dp, y = 11.dp)
+                    ) {
+                        Text(
+                            text = "Reviews",
+                            style = TextStyle(
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 25.sp,
+                                color = Color(0xFF5F5464)
+                            ),
+                            textAlign = TextAlign.Center
+                        )
+                    }
                 },
                 navigationIcon = {
                     IconButton(onClick = { onNavigateBack() }) {
@@ -131,13 +148,34 @@ fun CourseDetailsScreen(
                 .padding(innerPadding)
         ) {
             Spacer(modifier = Modifier.height(20.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                CustomShadowButton(
+                    text = "Teachers",
+                    selected = selectedTab == "Teachers",
+                    onClick = { selectedTab = "Teachers" }
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                CustomShadowButton(
+                    text = "Courses",
+                    selected = selectedTab == "Courses",
+                    onClick = { selectedTab = "Courses" }
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
 
             // Course Card
+// Course Card içerisindeki düzenlemeler
+            // Course Details Card içerisine öğretmen ismini dersin adı altına taşıdık.
             Card(
                 modifier = Modifier
                     .width(380.dp)
                     .height(169.dp)
-                    .padding(horizontal = 16.dp)
+                    .padding(2.dp)  // Dış boşluk
                     .align(Alignment.CenterHorizontally),
                 shape = RoundedCornerShape(10.dp),
                 colors = CardDefaults.cardColors(containerColor = Color(0xFFF9F9F9)),
@@ -146,69 +184,130 @@ fun CourseDetailsScreen(
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(16.dp)
+                        .padding(8.dp)
                 ) {
-                    // Course Code Box
-                    Box(
-                        modifier = Modifier
-                            .width(116.dp)
-                            .height(46.dp)
-                            .background(Color(0xFF85C0FF), RoundedCornerShape(8.dp)),
-                        contentAlignment = Alignment.Center
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
-                        Text(
-                            text = course.code,
-                            style = TextStyle(
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 22.sp,
-                                color = Color.White
+                        // Course Code Box
+                        Box(
+                            modifier = Modifier
+                                .width(116.dp)
+                                .height(46.dp)
+                                .background(Color(0xFF85C0FF), RoundedCornerShape(8.dp)),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = course.code,
+                                style = TextStyle(
+                                    fontFamily = OpenSansSemiBold,
+                                    fontWeight = FontWeight.SemiBold,
+                                    fontSize = 22.sp,
+                                    color = Color.White
+                                )
                             )
-                        )
+                        }
+
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        // Course Name
+                        Column(modifier = Modifier.weight(1f)) {
+                            Text(
+                                text = course.name,
+                                style = TextStyle(
+                                    fontFamily = OpenSansSemiBold,
+                                    fontWeight = FontWeight.SemiBold,
+                                    fontSize = 16.sp,
+                                    color = Color(0xFF342E37)
+                                ),
+                                maxLines = 2, // İki satırda yazması için
+                                overflow = TextOverflow.Ellipsis
+                            )
+                        }
                     }
 
                     Spacer(modifier = Modifier.height(8.dp))
 
-                    // Course Name and Instructor
-                    Text(
-                        text = course.name,
-                        style = TextStyle(
-                            fontWeight = FontWeight.SemiBold,
-                            fontSize = 16.sp,
-                            color = Color(0xFF342E37)
-                        )
-                    )
-
-                    Spacer(modifier = Modifier.height(4.dp))
-
-                    Text(
-                        text = course.instructor,
-                        style = TextStyle(
-                            fontWeight = FontWeight.Medium,
-                            fontSize = 14.sp,
-                            color = Color(0xFF707070)
-                        )
-                    )
+                    // Stars under the blue box
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Start // Sol hizalama
+                    ) {
+                        Spacer(modifier = Modifier.width(16.dp)) // Mavi kutunun hizasında olması için
+                        repeat(5) { index ->
+                            val starIcon = when {
+                                index < course.rating.toInt() -> R.drawable.ic_star_filled
+                                index == course.rating.toInt() && course.rating % 1f != 0f -> R.drawable.ic_star_half
+                                else -> R.drawable.ic_star_outline
+                            }
+                            Icon(
+                                painter = painterResource(id = starIcon),
+                                contentDescription = null,
+                                tint = Color(0xFF5BC658),
+                                modifier = Modifier.size(19.76.dp)
+                            )
+                        }
+                    }
 
                     Spacer(modifier = Modifier.height(8.dp))
 
-                    // Rate Course Button
-                    TextButton(
-                        onClick = onRateCourseClick,
-                        modifier = Modifier.align(Alignment.End)
-                    ) {
+
+                    // Öğretmen ismini kartın ortasında ve üste yakın konumlandırıyoruz
                         Text(
-                            text = "Rate Course",
+                            text = course.instructor,
                             style = TextStyle(
-                                fontWeight = FontWeight.Bold,
+                                fontFamily = OpenSansSemiBold,
+                                fontWeight = FontWeight.SemiBold,
                                 fontSize = 14.sp,
-                                color = Color(0xFF3C9BFF)
-                            )
+                                color = Color(0xFF5BC658)
+                            ),
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally) // Ortada hizalama
+                                .offset(y = (-40).dp) // Yukarıya taşıma
                         )
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    // Rate Course Button
+                    Button(
+                        onClick = onRateCourseClick,
+                        modifier = Modifier
+                            .align(Alignment.End)  // Butonu sağa hizalıyoruz
+                            .padding(2.dp),
+                        shape = RoundedCornerShape(20),  // Butonun köşeleri yuvarlatılmış
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent // Arka planı şeffaf yapıyoruz
+                        )
+                    ) {
+                        Row {
+                            Text(
+                                text = "Rate Course",  // Buton metni
+                                style = TextStyle(
+                                    fontFamily = OpenSansSemiBold,
+                                    fontWeight = FontWeight.SemiBold,
+                                    fontSize = 13.sp,
+                                    color = Color(0xFF5BC658)  // Yeşil renkli metin
+                                )
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))  // Metin ve ikon arasına boşluk ekliyoruz
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_arrow_right),  // Ok simgesi
+                                contentDescription = "Arrow Right",
+                                tint = Color(0xFF5BC658),  // İkonun yeşil rengi
+                                modifier = Modifier.size(18.dp)  // İkon boyutu
+                            )
+                        }
                     }
                 }
             }
 
-            Spacer(modifier = Modifier.height(16.dp))
+
+
+
+
+
+
+            Spacer(modifier = Modifier.height(10.dp))
 
             // Reviews List
             LazyColumn(modifier = Modifier.fillMaxSize()) {
@@ -285,7 +384,7 @@ val dummyCourse2 = Course(
     id = 2,
     code = "VCD 592",
     name = "Internship",
-    rating = 3f,
+    rating = 2.8f,
     instructor = "Murat Yılmaz",
     reviews = listOf(
         Review(4, "Anonim", "Ders çok faydalı.", 4, courseId = 2),
@@ -301,4 +400,27 @@ fun TestCourseDetailsScreen() {
         onNavigateBack = { /* Geri gitme işlevi */ },
         onRateCourseClick = { /* Rate course işlevi */ }
     )
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewCourseReviewItem() {
+    val dummyReview = Review(1, "Anonim", "Ders baya iyi.", 5, courseId = 1)
+    CourseReviewItem(review = dummyReview)
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewCourseDetailsScreen() {
+    CourseDetailsScreen(
+        course = dummyCourse1,
+        onNavigateBack = { /* Geri gitme işlevi */ },
+        onRateCourseClick = { /* Rate course işlevi */ }
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewCourseReviewScreen() {
+    CourseReviewScreen(onCourseClick = { courseId -> /* Course click işlevi */ })
 }
